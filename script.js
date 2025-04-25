973d8222473e3b4125310a3be4d4bce4162ce22f
L <simonlauko02@gmail.com>
2025-04-25 09:34:59 +0200

    typing effect repair

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const titleText = "Prihláste sa ";
-    const headerText = "IQ Securitas 4000 ";
-    const dvereNadpis = "Detailné logy z dverí ";
-    const oknaNadpis = "Detailné logy z okien ";
-    const senzorNadpis = "Detailné logy senzora ";
+    const titleText = "Prihláste sa";
+    const headerText = "IQ Securitas 4000";
+    const dvereNadpis = "Detailné logy z dverí";
+    const oknaNadpis = "Detailné logy z okien";
+    const senzorNadpis = "Detailné logy senzora";
 
     let titleIndex = 0;
     let headerIndex = 0;
@@ -15,35 +15,35 @@ document.addEventListener("DOMContentLoaded", function () {
         const titleElem = document.getElementById("typing-title");
         if (!titleElem) return;
         titleElem.textContent = titleText.slice(0, titleIndex++);
-        if (titleIndex < titleText.length) setTimeout(typeTitle, 250);
+        if (titleIndex <= titleText.length) setTimeout(typeTitle, 250);
     }
 
     function typeHeader() {
         const headerElem = document.getElementById("header");
         if (!headerElem) return;
         headerElem.textContent = headerText.slice(0, headerIndex++);
-        if (headerIndex < headerText.length) setTimeout(typeHeader, 150);
+        if (headerIndex <= headerText.length) setTimeout(typeHeader, 150);
     }
 
     function typeDvere() {
         const titleElem = document.getElementById("dvereheader");
         if (!titleElem) return;
         titleElem.textContent = dvereNadpis.slice(0, dvereIndex++);
-        if (dvereIndex < dvereNadpis.length) setTimeout(typeDvere, 250);
+        if (dvereIndex <= dvereNadpis.length) setTimeout(typeDvere, 250);
     }
 
     function typeOkno() {
         const titleElem = document.getElementById("oknoheader");
         if (!titleElem) return;
         titleElem.textContent = oknaNadpis.slice(0, oknoIndex++);
-        if (oknoIndex < oknaNadpis.length) setTimeout(typeOkno, 250);
+        if (oknoIndex <= oknaNadpis.length) setTimeout(typeOkno, 250);
     }
 
     function typeSenzor() {
         const titleElem = document.getElementById("senzorheader");
         if (!titleElem) return;
         titleElem.textContent = senzorNadpis.slice(0, senzorIndex++);
-        if (senzorIndex < senzorNadpis.length) setTimeout(typeSenzor, 250);
+        if (senzorIndex <= senzorNadpis.length) setTimeout(typeSenzor, 250);
     }
 
     typeTitle();
